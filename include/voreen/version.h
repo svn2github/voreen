ec65928cf9d68915c71496d15ce15ec83aae5747
mensmann <mensmann@d84f7722-cf57-0410-9e8f-1be463e99fdd>
2008-12-05 13:15:40 +0000

    fix display problems on Vista and with some ATI cards (triple buffering)
    git-svn-id: http://svn.voreen.org/public/voreen-snapshot@10 d84f7722-cf57-0410-9e8f-1be463e99fdd

--- include/voreen/version.h
+++ include/voreen/version.h
@@ -32,7 +32,7 @@
 
 #ifdef VRN_SNAPSHOT
   // update version for each snapshot release
-  #define VRN_SVN_REVISON "snapshot-9"
+  #define VRN_SVN_REVISON "snapshot-10"
 #endif
 
 #ifdef VRN_WITH_SVNVERSION
